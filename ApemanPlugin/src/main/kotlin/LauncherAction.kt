c62953645d7f9a20159c7726d93a9f61db6765a6
Gleb Zakharov <snyssfx@gmail.com>
2018-11-13 20:37:15 +0300

    LauncherAction: add inheritance from AnalyzeAction, add working example of metrics extraction

--- ApemanPlugin/src/main/kotlin/LauncherAction.kt
+++ ApemanPlugin/src/main/kotlin/LauncherAction.kt
@@ -1,16 +1,40 @@
+import com.intellij.analysis.AnalysisScope
+import com.intellij.analysis.BaseAnalysisAction
 import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.fileChooser.tree.FileNodeVisitor
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.modifyModules
+import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiManager
 
-class LauncherAction : AnAction ("Check current file"){
+class LauncherAction : BaseAnalysisAction("check1", "check2") {
 
-    override fun actionPerformed(e: AnActionEvent?) {
-        val project = e?.project
-        val file = e?.getData(LangDataKeys.PSI_FILE)
+    override fun analyze(project: Project, scope: AnalysisScope) {
 
-        if (file == null || project == null) return
+        if (scope.fileCount == 0) {
+            return
+        }
 
-        val candidates = CandidatesOfFile(file).candidates
+        val features = FeaturesForEveryCandidate(project, scope)
 
-        Messages.showMessageDialog(project, candidates.joinToString(separator = "\n"), "Checking methods", Messages.getInformationIcon())
+        var info = ""
+        val measuredObjects = features.resultsForMethods?.measuredObjects ?: return
+        val metrics = features.resultsForMethods?.metrics ?: return
+
+        for (obj in measuredObjects) {
+            info += "$obj:\n"
+
+            for (metric in metrics) {
+                val results = features.resultsForMethods?.getValueForMetric(metric, obj)
+                info += "${metric.displayName}: $results\n"
+            }
+
+            info += "\n\n"
+        }
+
+        Messages.showInfoMessage(info, "checked")
     }
 }
