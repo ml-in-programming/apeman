bed775ba1accf210d9406bc67377dab76fe9bd2d
Gleb Zakharov <snyssfx@gmail.com>
2018-11-21 19:15:43 +0300

    MetricsReloaded: add NumTernaryOperatorsCandidateMetric

--- ApemanPlugin/src/main/kotlin/FeaturesForEveryCandidate.kt
+++ ApemanPlugin/src/main/kotlin/FeaturesForEveryCandidate.kt
@@ -1,10 +1,7 @@
 import com.intellij.analysis.AnalysisScope
-import com.intellij.codeInspection.GlobalInspectionContext
-import com.intellij.codeInspection.ex.GlobalInspectionContextImpl
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.util.ProgressIndicatorBase
 import com.intellij.openapi.project.Project
-import com.intellij.psi.search.GlobalSearchScopesCore
 import com.sixrr.metrics.Metric
 import com.sixrr.metrics.MetricCategory
 import com.sixrr.metrics.metricModel.*
@@ -12,6 +9,7 @@ import com.sixrr.metrics.profile.MetricInstanceImpl
 import com.sixrr.metrics.profile.MetricsProfile
 import com.sixrr.metrics.profile.MetricsProfileImpl
 import com.sixrr.stockmetrics.candidateMetrics.*
+import com.sixrr.stockmetrics.methodMetrics.*
 import org.jetbrains.research.groups.ml_methods.utils.ExtractionCandidate
 
 class FeaturesForEveryCandidate(project: Project, var analysisScope: AnalysisScope,
@@ -19,18 +17,19 @@ class FeaturesForEveryCandidate(project: Project, var analysisScope: AnalysisSco
 
     private val gemsMetrics = listOf<Metric>(
 
-            com.sixrr.stockmetrics.methodMetrics.NumLocalVarsMetric(),
-            com.sixrr.stockmetrics.methodMetrics.NumLiteralsMetric(),
-            com.sixrr.stockmetrics.methodMetrics.NumIfMetric(),
-            com.sixrr.stockmetrics.methodMetrics.NumAssertsMetric(),
-            com.sixrr.stockmetrics.methodMetrics.NumAssignmentsMetric(),
-            com.sixrr.stockmetrics.methodMetrics.NumUsedTypesMetric(),
-            com.sixrr.stockmetrics.methodMetrics.NumUsedPackagesMetric(),
-            com.sixrr.stockmetrics.methodMetrics.NumMethodCallsMetric(),
-            com.sixrr.stockmetrics.methodMetrics.NumLocalVarsAccessMetric(),
+            NumLocalVarsMetric(),
+            NumLiteralsMetric(),
+            NumTernaryOperatorsMetric(),
+            NumAssertsMetric(),
+            NumAssignmentsMetric(),
+            NumUsedTypesMetric(),
+            NumUsedPackagesMetric(),
+            NumMethodCallsMetric(),
+            NumLocalVarsAccessMetric(),
 
             // candidates metrics
-            NumLiteralsMetric(candidates)
+            NumLiteralsCandidateMetric(candidates),
+            NumTernaryOperatorsCandidateMetric(candidates)
     )
 
     private val metricsProfile: MetricsProfile = MetricsProfileImpl("Gems metrics",
