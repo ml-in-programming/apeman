084d7f738fe95484132bd9fe95d85822f6766045
Gleb Zakharov <snyssfx@gmail.com>
2019-03-01 22:35:43 +0300

    add Features enum

--- ApemanPlugin/src/main/kotlin/apeman_core/pipes/CandidateWithFeatures.kt
+++ ApemanPlugin/src/main/kotlin/apeman_core/pipes/CandidateWithFeatures.kt
@@ -4,5 +4,5 @@ import org.jetbrains.research.groups.ml_methods.utils.ExtractionCandidate
 
 data class CandidateWithFeatures(
         val candidate: ExtractionCandidate,
-        val features: List<Feature>
+        val features: Features
 )
--- ApemanPlugin/src/main/kotlin/apeman_core/pipes/CandidatesWithFeaturesAndProba.kt
+++ ApemanPlugin/src/main/kotlin/apeman_core/pipes/CandidatesWithFeaturesAndProba.kt
@@ -4,6 +4,6 @@ import org.jetbrains.research.groups.ml_methods.utils.ExtractionCandidate
 
 data class CandidatesWithFeaturesAndProba(
         val candidate: ExtractionCandidate,
-        val features: List<Feature>,
+        val features: Features,
         val probability: Double
 )
